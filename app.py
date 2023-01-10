039d8cea785f2c7e8d6ce30ba89c1624a5933a6c
iv_note <ivan_note@local.ru>
2023-01-10 22:45:47 +0100

    add settings for ssl nginx

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
+
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World! Я тебя запустил!'
+
+
 
 
 if __name__ == '__main__':
